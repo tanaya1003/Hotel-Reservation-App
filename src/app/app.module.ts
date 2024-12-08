d663a482dfd8070ff7e8869595db319491a5c0e1
Tanaya10-dotcom <tiwatnetanaya@gmail.com>
2024-12-08 20:31:32 +0530

    Version 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ReservationModule } from './reservation/reservation.module';
+import { provideHttpClient} from '@angular/common/http'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    ReservationModule,
+    
+  
+  ],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient()
+    
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
